0961fcac7e9ea00fcf9feb202eb5d3128a5bfcf4
FabioOnGithub <83075330+FabioOnGithub@users.noreply.github.com>
2023-08-20 18:22:40 +0200

    Update descope.js

--- static/descope.js
+++ static/descope.js
@@ -1,3 +1,3 @@
-const projectId = ""
+const projectId = "P2UFtkOPBYUU9qKofYYR77EnA1rl"
 const sdk = Descope({ projectId: projectId, persistTokens: true, autoRefresh: true })
 const sessionToken = sdk.getSessionToken()
